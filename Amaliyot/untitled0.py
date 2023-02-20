75a2e52b1ba8e24b67013379b956e38802fe7152
Adham Isoqov <94904141+AdhamIsoqov@users.noreply.github.com>
2023-02-20 11:22:32 +0500

    PYTHON
    yangi amaliyot javoblari

---
+++ Amaliyot/untitled0.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 20 00:15:07 2023
+
+@author: Lider_Isoqov
+"""
+# =============================================================================
+# otam = {
+#         'ism' : 'abdunabi',
+#         'familiya' : 'rahmonov',
+#         't_yil': 1974,
+#         'yosh': 48,
+#         'shahar' : 'jizzax',
+#         'tuman' : "g'allaorol",
+#         'mahalla' : 'jabosh',
+#         "kocha" : "obod yo'l",
+#         'uy' : 7,
+#         }
+# onam = {
+#         'ism' : 'dilfuza',
+#         'familiya' : 'rahmonov',
+#         't_yil' : 1978,
+#         'yosh': 44,
+#         'shahar' : 'jizzax',
+#         'tuman' : "g'allaorol",
+#         'mahalla' : 'jabosh',
+#         "kocha" : "obod yo'l",
+#         'uy' : 7,
+#         }
+# opcham = {
+#         'ism' : 'dilobar',
+#         'familiya' : 'isoqov',
+#         't_yil' : 1999,
+#         'yosh': 23,
+#         'shahar' : 'jizzax',
+#         'tuman' : "g'allaorol",
+#         'mahalla' : 'jabosh',
+#         "kocha" : "obod yo'l",
+#         'uy' : 7,
+#         }
+# akam = {
+#         'ism' : "ro'zimurod",
+#         'familiya' : 'isoqov',
+#         't_yil' : 2000,
+#         'yosh': 22,
+#         'shahar' : 'jizzax',
+#         'tuman' : "g'allaorol",
+#         'mahalla' : 'jabosh',
+#         "kocha" : "obod yo'l",
+#         'uy' : 7,
+#         }
+# men = {
+#         'ism' : 'adham',
+#         'familiya' : 'isoqov',
+#         't_yil' : 2004,
+#         'yosh': 18,
+#         'shahar' : 'jizzax',
+#         'tuman' : "g'allaorol",
+#         'mahalla' : 'jabosh',
+#         "kocha" : "obod yo'l",
+#         'uy' : 7,
+#         }
+# singlim = {
+#         'ism' : 'nozima',
+#         'familiya' : 'isoqov',
+#         't_yil' : 2012,
+#         'yosh': 10,
+#         'shahar' : 'jizzax',
+#         'tuman' : "g'allaorol",
+#         'mahalla' : 'jabosh',
+#         "kocha" : "obod yo'l",
+#         'uy' : 7,
+#         }
+# 
+# print(f'Otamning ismi {otam["ism"].title()}, {otam["yosh"]}-yilda, {otam["shahar"].title()}da tu\'ilgan. Hozirda {otam["shahar"].title()} shahri {otam["tuman"].title()} tumani {otam["mahalla"].title()} qishlog\'i {otam["kocha"].title()} ko\'chasi {otam["uy"]}-uyda yashaydi.\n\n')
+# print(f'Onamning ismi {onam["ism"].title()}, {onam["yosh"]}-yilda, {onam["shahar"].title()}da tu\'ilgan. Hozirda {onam["shahar"].title()} shahri {onam["tuman"].title()} tumani {onam["mahalla"].title()} qishlog\'i {onam["kocha"].title()} ko\'chasi {onam["uy"]}-uyda yashaydi.\n\n')
+# print(f'Opchamning ismi {opcham["ism"].title()}, {opcham["yosh"]}-yilda, {opcham["shahar"].title()}da tu\'ilgan. Hozirda {opcham["shahar"].title()} shahri {opcham["tuman"].title()} tumani {opcham["mahalla"].title()} qishlog\'i {opcham["kocha"].title()} ko\'chasi {opcham["uy"]}-uyda yashaydi.\n\n')
+# print(f'Akamning ismi {akam["ism"].title()}, {akam["yosh"]}-yilda, {akam["shahar"].title()}da tu\'ilgan. Hozirda {akam["shahar"].title()} shahri {akam["tuman"].title()} tumani {akam["mahalla"].title()} qishlog\'i {akam["kocha"].title()} ko\'chasi {akam["uy"]}-uyda yashaydi.\n')
+# print(f'Menning ismi {men["ism"].title()}, {men["yosh"]}-yilda, {men["shahar"].title()}da tu\'ilgan. Hozirda {men["shahar"].title()} shahri {men["tuman"].title()} tumani {men["mahalla"].title()} qishlog\'i {men["kocha"].title()} ko\'chasi {men["uy"]}-uyda yashaydi.\n')
+# print(f'Singlimning ismi {singlim["ism"].title()}, {singlim["yosh"]}-yilda, {singlim["shahar"].title()}da tu\'ilgan. Hozirda {singlim["shahar"].title()} shahri {singlim["tuman"].title()} tumani {singlim["mahalla"].title()} qishlog\'i {singlim["kocha"].title()} ko\'chasi {singlim["uy"]}-uyda yashaydi.')
+# =============================================================================
+
+# =============================================================================
+# taomlar = {
+#     'otam' : 'manti',
+#     'onam' : 'somsa',
+#     'opam' : 'kabob',
+#     'akam' : 'steke',
+#     'men' : 'kabob',
+#     'singlim' : 'somsa'
+#     }
+# print(f'Otamning yoqtirgan taomi {taomlar["otam"]}')
+# print(f'Onamning yoqtirgan taomi {taomlar["onam"]}')
+# print(f'Opamning yoqtirgan taomi {taomlar["opam"]}')
+# =============================================================================
+
+# =============================================================================
+# dictonary_python = {
+#     'integer' : 'butun son',
+#     'float' : 'kasr son',
+#     'string' : 'matnlar',
+#     'if' : 'agar',
+#     'else' : 'aks holda',
+#     'for' : 'uchun'
+#     }
+# print(dictonary_python)
+# =============================================================================
+
+
+# =============================================================================
+# dictonary_python = {
+#     'integer' : 'butun son',
+#     'float' : 'kasr son',
+#     'string' : 'matnlar',
+#     'if' : 'agar',
+#     'else' : 'aks holda',
+#     'for' : 'uchun'
+#     }
+# print(dictonary_python)
+# key_dictonary = input('Kalit so`z kiritng: ')
+# dictonary = dictonary_python.get( key_dictonary, 'Bunda so\'z mavjud emas')
+# print(dictonary)
+# =============================================================================
+
+dictonary_python = {
+    'integer' : 'butun son',
+    'float' : 'kasr son',
+    'string' : 'matnlar',
+    'if' : 'agar',
+    'else' : 'aks holda',
+    'for' : 'uchun'
+    }
+key_dictonary = input('Kalit so`z kiritng: ')
+if key_dictonary in dictonary_python:
+    print(dictonary_python[key_dictonary])
+else:
+    print('bunday so\'z mavjud emas.')
+
+
+
+
+
+
+
+
+
+
+
+
+
